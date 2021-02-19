e1a4136dff4d6d6c05b436bb82b26ce60799190d
LAPTOP-1TVEU805\Owner <kuri222@naver.com>
2021-02-19 23:10:58 +0900

    chap01_1

---
+++ Chap01/src/chap01_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class chap01_1 {
+	
+		static int max4(int a, int b, int c, int d) {
+			int max = a; 	
+			if (b > max)
+				max = b;
+			if (c > max)
+				max = c;
+			if (d > max)
+				max = d;
+
+			return max;
+		}
+
+		public static void main(String[] args) {
+			Scanner stdIn = new Scanner(System.in);
+			int a, b, c, d;
+
+			System.out.println("네 값의 최댓값을 구하시오.");
+			System.out.print("a의 값：");
+			a = stdIn.nextInt();
+			System.out.print("b의 값：");
+			b = stdIn.nextInt();
+			System.out.print("c의 값：");
+			c = stdIn.nextInt();
+			System.out.print("d의 값：");
+			d = stdIn.nextInt();
+
+			int max = max4(a, b, c, d); 	
+
+			System.out.println("최댓값은 " + max + "입니다.");
+		}
+}
