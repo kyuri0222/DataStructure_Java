b3d11521a6be62d97294a28a7a3501c314e0728f
Kyuri Kim <kuri222@naver.com>
2022-04-16 16:44:55 +0900

    Week6

---
+++ Week6/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+	public static void main(String[] args) {
+		ArrayQueue<String> queue = new ArrayQueue<String>();
+		
+		// queue.remove();
+		queue.add("apple");
+		queue.add("orange");
+		queue.add("cherry");
+		queue.add("pear"); queue.print();
+		
+		queue.remove(); queue.print();
+		
+ 		queue.add("grape"); queue.print();
+ 		
+		queue.remove(); queue.print();
+	
+		queue.add("lemon"); queue.print();
+		queue.add("mango"); queue.print();
+		queue.add("lime"); queue.print();
+		queue.add("kiwi"); queue.print();
+		
+		queue.remove(); queue.print();
+	}
+}
