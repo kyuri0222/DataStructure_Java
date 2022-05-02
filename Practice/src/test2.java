6a81e61113619b405ea1162a4b234b1b63d69f2c
Kyuri Kim <kuri222@naver.com>
2022-05-02 10:01:22 +0900

    Practice

---
+++ Practice/src/test2.java
@@ -0,0 +1,102 @@
+
+public class test2 {
+
+}
+
+/*
+🔥
+comparable – x.compareTo(y) – 학번
+- 하나의 멤버를 기준으로 비교
+
+public interface Comparable {
+	public int compareTo(T other);
+}
+public class 클래이름 implements Comparable<클래스이름> {
+	public int compareTo(클래스 이름 other )}
+
+🔥
+comparator – compare(x,y) – 이름 학과이름 학년
+- 여러 멤버를 기준으로 나열
+import java.util.Comparator;
+
+public interface Comparator{
+	piblic int compare(T first, T second);
+}
+
+public class 클래스이름 implments Comparator<클래스이름>{
+	public int compare(클래스이름 first,클래스이름 second)}
+*/
+
+
+/*
+ * import java.util.Comparator;
+ *
+ * public class Student implements Comparable<Student>{}
+ * 	public int CompareTo (Student s){
+ *  	return this.id-s.id;
+ * 
+ * public static Comparator<Student> CompName = new Comparator<Student>() {
+ * 	public int compare(Student s1, Student s2){
+ * 		return s1.name.compareTo(s2.name);
+ * 
+ * public static Comparator<Student> CompDept = new Comparator<Student>(){
+ * 	public int compare(Student s1, Student s2){
+ * 		return s1.dept.compareTo(s2.dept);
+ * 
+ * public static Comparator<Student> CompGrade = new Comparator<Student>(){
+ * 	public int compare(Student s1, Student s2){
+ * 		return s1.grade-s2.grade;
+ 
+ * public class Factorial {
+ * 	public static int factorial(int n){
+	if (n<=1) return 1;
+	else
+	return n*factorial(n-1)'
+	
+ * public static void main (String[] args){
+ * int result;
+ * result = factorial (4);
+ * System.out.print(result;)}
+ * }
+ * 
+ * public class Fatorial{
+ * public static void main(String[] args){
+int n=4
+ins result = 1
+for (i=0; i<=n; i++){}
+result = result * i
+
+hanoi (int n, char from, char by, char to){
+if(n=1)
+sys.out.printf(1을 -에서 -로 이동,from, to)
+else
+hanoi (n-1,from,to,by)
+sys.out.printf(1을 -에서 -로 이동,from, to)
+hanoi(n-1, by, from , to)
+}
+
+if (N!=null){
+sys.out.printf(n.name)
+A_course(n,left)
+A.Course(n,right)
+
+
+if (N!=null){
+B_Course(n.left)
+sys.out.printf(n.name)
+B_Course(n.right)
+
+if (N!=null){
+C_Course(n.left)
+C_Course(n.right)
+sys.out.printf(n.name)
+}
+}
+
+N
+
+ 
+ 
+ 
+ 
+ * */
